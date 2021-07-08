2308053d2d97e846c014b8583f44fda2c964fb6d
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2021-07-08 21:42:51 +0100

    s

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.route('/')
-def index():
-    return 'hey'
--- run.py
+++
@@ -1,5 +0,0 @@
-import os
-
-if __name__ == '__main__':
-    os.system('uvicorn main:app --reload')
-    pass
