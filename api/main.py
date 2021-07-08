dc1d1b511c14af1356dab5a90a3b2fb786188cff
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2021-07-08 13:23:13 +0100

    update

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import  models
 from .database import engine
-from  .routers import blog, user, authentication
+from  .routers import blog, user, authentication, model
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(engine)
 app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+app.include_router(model.router)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
 from .database import Base
 from sqlalchemy.orm import relationship
 
@@ -14,15 +14,39 @@ class Blog(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     creator = relationship("User", back_populates="blogs")
 
-
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    email = Column(String)
+    email = Column(String, unique=True)
     password = Column(String)
     blogs = relationship('Blog', back_populates="creator")
+    model_list = relationship("Model", back_populates="id")
 
 class Model(Base):
     __tablename__ = 'model'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    python_script_path = Column(String, nullable=False)
+    input_type = Column(String, nullable=False)
+    output_type = Column(String, nullable=False)
+    is_private = Column(Boolean, default=True, nullable=False)
+    created_in = Column(DateTime, nullable=False)
+    paths = relationship("Path", back_populates="id")
+    user_list = relationship("User", back_populates="id")
+
+class Path(Base):
+    __tablename__ = 'path'
+
+    id = Column(Integer, primary_key=True, index=True)
+    path = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    models = relationship("Model", back_populates="id")
+
+
+
+
+
