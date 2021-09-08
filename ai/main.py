3c24feb10446d22fa23bc531237289f693fd63d5
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2021-09-08 17:23:45 +0100

    Update main.py

--- ai/main.py
+++ ai/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["content-disposition"]
 )
 
 #mount the static folder to get the brain logo in html
