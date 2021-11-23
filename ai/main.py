051ddb7b958929284073b859003291667b68d7b8
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2021-11-23 23:01:41 +0000

    more auth 0 stuff

--- ai/main.py
+++ ai/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from . import models
@@ -106,8 +106,46 @@ async def file_cleanup():
                     print("FILE PATH: ", fil, "\\ LAST ACCESS TIME: ", time.ctime(last_access_time), "\\ DELETE TIME: ", time.ctime(present_time))
                     print("DELETING DIRECTORY: ", dir_path)
                     shutil.rmtree(dir_path)
-            
+
+# test for auth0
+from fastapi import Depends, Response, status
+from fastapi.security import HTTPBearer
+from .utils import VerifyToken
+
+token_auth_scheme = HTTPBearer()
+
+@app.get("/api/public")
+def public():
+    """No access token required to access this route"""
+
+    result = {
+        "status": "success",
+        "msg": ("Hello from a public endpoint! "
+                "You don't need to be authenticated to see this.")
+    }
+    return result
+
+
+@app.get("/api/private")
+def private(response: Response, token: str = Depends(token_auth_scheme)):
+    """A valid access token is required to access this route"""
     
+    
+    
+    result = VerifyToken(token.credentials).verify()
+    
+    if result.get("status"):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return result
+
+    """ result = {
+        "status": "success",
+        "msg": ("Hello from a private endpoint! "
+                "You need to be authenticated to see this.")
+    } """
+    
+    return result
+
 
 @app.get("/")
 async def main():
