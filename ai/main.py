4276cdc4136a3711d4a074e8365270a00ab9aec6
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2021-11-24 21:04:30 +0000

    Update main.py

--- ai/main.py
+++ ai/main.py
@@ -72,42 +72,45 @@ async def insert_initial_values(db: Session = Depends(get_db)):
 
 #delete files that haven't been accessed in 24h, checked every 24h since server start
 @app.on_event("startup")
-@repeat_every(seconds = 60 * 24 * 60) #repeat every hour
+@repeat_every(seconds = 60) #repeat every hour
 async def file_cleanup():
-    print("Cleaning old files...")
-    path1 = ".\inputfiles"
-    path2 = ".\outputfiles"
-    max_access_time = 60 * 24 * 60
-    present_time=time.time()
-    if os.path.exists(path1):
-        for (roots, dirs, files) in os.walk(path1):
-            for f in files:
-                fil=os.path.join(roots,f)
-                fil_stat=os.stat(fil)
-                last_access_time=fil_stat.st_atime
-                if last_access_time < present_time-max_access_time:
-                    fil_split = fil.split("\\")
-                    dir_path = fil_split[0] + "\\" + fil_split[1] + "\\" + fil_split[2]
-                    print(dir_path)
-                    print("FILE PATH: ", fil, "\\ LAST ACCESS TIME: ", time.ctime(last_access_time), "\\ DELETE TIME: ", time.ctime(present_time))
-                    print("DELETING DIRECTORY: ", dir_path)
-                    shutil.rmtree(dir_path)
-    if os.path.exists(path2):
-        for (roots, dirs, files) in os.walk(path2):
-            for f in files:
-                fil=os.path.join(roots,f)
-                fil_stat=os.stat(fil)
-                last_access_time=fil_stat.st_atime
-                """ print(fil, time.ctime(last_access_time)) """
-                if last_access_time < present_time-max_access_time:
-                    fil_split = fil.split("\\")
-                    dir_path = fil_split[0] + "\\" + fil_split[1] + "\\" + fil_split[2]
-                    print(dir_path)
-                    print("FILE PATH: ", fil, "\\ LAST ACCESS TIME: ", time.ctime(last_access_time), "\\ DELETE TIME: ", time.ctime(present_time))
-                    print("DELETING DIRECTORY: ", dir_path)
-                    shutil.rmtree(dir_path)
-            
-    
+    try: 
+        print("Cleaning old files...")
+        path1 = ".\inputfiles"
+        path2 = ".\outputfiles"
+        max_access_time = 60
+        present_time=time.time()
+        if os.path.exists(path1):
+            for (roots, dirs, files) in os.walk(path1):
+                for f in files:
+                    fil=os.path.join(roots,f)
+                    fil_stat=os.stat(fil)
+                    last_access_time=fil_stat.st_atime
+                    if last_access_time < present_time-max_access_time:
+                        print("here")
+                        fil_split = fil.split("\\")
+                        print(fil_split)
+                        dir_path = fil_split[0] + "\\" + fil_split[1] + "\\" + fil_split[2]
+                        print(dir_path)
+                        print("FILE PATH: ", fil, "/ LAST ACCESS TIME: ", time.ctime(last_access_time), "/ DELETE TIME: ", time.ctime(present_time))
+                        print("DELETING DIRECTORY: ", dir_path)
+                        shutil.rmtree(dir_path)
+        if os.path.exists(path2):
+            for (roots, dirs, files) in os.walk(path2):
+                for f in files:
+                    fil=os.path.join(roots,f)
+                    fil_stat=os.stat(fil)
+                    last_access_time=fil_stat.st_atime
+                    """ print(fil, time.ctime(last_access_time)) """
+                    if last_access_time < present_time-max_access_time:
+                        fil_split = fil.split("\\")
+                        dir_path = fil_split[0] + "\\" + fil_split[1] + "\\" + fil_split[2]
+                        print(dir_path)
+                        print("FILE PATH: ", fil, "/ LAST ACCESS TIME: ", time.ctime(last_access_time), "/ DELETE TIME: ", time.ctime(present_time))
+                        print("DELETING DIRECTORY: ", dir_path)
+                        shutil.rmtree(dir_path)
+    except:
+        print("error")
 
 @app.get("/")
 async def main():
