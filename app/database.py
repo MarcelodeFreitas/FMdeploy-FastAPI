20126fc38d36a821f436776186457ff3ee5bd9ff
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2022-01-01 20:13:26 +0000

    postgres connection

--- app/database.py
+++ app/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db'
+""" SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db' """
+SQLALCHAMY_DATABASE_URL = 'postgresql://postgres:2f2kir2y49xy6g@localhost/fmdeploy_db'
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+engine = create_engine(SQLALCHAMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
