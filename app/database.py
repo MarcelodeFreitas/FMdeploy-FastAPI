2270a0cb3cafaaa29372742d9aebc74f4d50a9b3
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2022-01-13 19:38:45 +0000

    first

--- app/database.py
+++ app/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db'
+""" SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db' """
+""" SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db' """
+""" SQLALCHAMY_DATABASE_URL = 'postgresql://postgres:2f2kir2y49xy6g@localhost/fmdeploy_db' """
+SQLALCHAMY_DATABASE_URL = 'mysql+mysqlconnector://root:1875mdfm@localhost:3306/fmdeploy_db'
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
                        "check_same_thread": False})
--- app/models.py
+++ app/models.py
@@ -10,11 +10,13 @@ class User(Base):
     __tablename__ = 'user'
     user_id = Column(Integer, primary_key=True, index=True)
     userailist = relationship("UserAIList")
-    name = Column(String)
-    email = Column(String, unique=True)
-    password = Column(String)
+    name = Column(String(length=255))
+    email = Column(String(length=255), unique=True)
+    password = Column(String(length=255))
     is_admin = Column(Boolean, default=False, nullable=True)
     
+""" event.listen(User.__table__, 'after_create',
+            DDL(" INSERT INTO \"user\" (name, email, password, is_admin) VALUES ('admin', 'fmdeploy@gmail.com', '$2b$12$cm7LbkGUMSzbWe9fAdCXJO/lzivm49UHi4aEGR21bpbQ5aX6a4hdS', TRUE) ")) """
 event.listen(User.__table__, 'after_create',
             DDL(" INSERT INTO user (name, email, password, is_admin) VALUES ('admin', 'fmdeploy@gmail.com', '$2b$12$cm7LbkGUMSzbWe9fAdCXJO/lzivm49UHi4aEGR21bpbQ5aX6a4hdS', TRUE) "))
     
@@ -22,21 +24,21 @@ class UserAIList(Base):
     __tablename__ = 'userailist'
     user_ai_list_id = Column(Integer, primary_key=True, index=True)
     fk_user_id = Column(Integer, ForeignKey('user.user_id'), nullable=False)
-    fk_ai_id = Column(String, ForeignKey('ai.ai_id'), nullable=False)
+    fk_ai_id = Column(String(length=255), ForeignKey('ai.ai_id'), nullable=False)
     owner =  Column(Boolean, default=False, nullable=False)
     
 class AI(Base):
     __tablename__ = 'ai'
-    ai_id = Column(String, primary_key=True, index=True)
+    ai_id = Column(String(length=255), primary_key=True, index=True)
     userailist = relationship("UserAIList")
     ai_files = relationship("ModelFile")
-    author = Column(String, nullable=False)
-    title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    input_type = Column(String, nullable=False)
-    output_type = Column(String, nullable=False)
-    python_script_name = Column(String, nullable=True)
-    python_script_path = Column(String, nullable=True)
+    author = Column(String(length=255), nullable=False)
+    title = Column(String(length=255), nullable=False)
+    description = Column(String(length=255), nullable=False)
+    input_type = Column(String(length=255), nullable=False)
+    output_type = Column(String(length=255), nullable=False)
+    python_script_name = Column(String(length=255), nullable=True)
+    python_script_path = Column(String(length=255), nullable=True)
     is_private = Column(Boolean, default=True, nullable=False)
     created_in = Column(DateTime, nullable=False)
     last_updated = Column(DateTime, nullable=True)
@@ -44,12 +46,12 @@ class AI(Base):
 class ModelFile(Base):
     __tablename__ = 'modelfile'
     model_file_id = Column(Integer, primary_key=True, index=True)
-    fk_ai_id = Column(String, ForeignKey('ai.ai_id'))
-    name = Column(String, nullable=False)
-    path = Column(String, nullable=False)
+    fk_ai_id = Column(String(length=255), ForeignKey('ai.ai_id'))
+    name = Column(String(length=255), nullable=False)
+    path = Column(String(length=255), nullable=False)
 
 class InputFile(Base):
     __tablename__ = 'inputfile'
-    input_file_id = Column(String, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    path = Column(String, nullable=False)
+    input_file_id = Column(String(length=255), primary_key=True, index=True)
+    name = Column(String(length=255), nullable=False)
+    path = Column(String(length=255), nullable=False)
