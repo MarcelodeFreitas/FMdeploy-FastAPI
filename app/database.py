e1376f6e979893189a1088e9c3dfe2145fbc4865
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2022-01-25 20:08:22 +0000

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,9 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-""" SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db' """
-""" SQLALCHAMY_DATABASE_URL = 'sqlite:///./database.db' """
-""" SQLALCHAMY_DATABASE_URL = 'postgresql://postgres:2f2kir2y49xy6g@localhost/fmdeploy_db' """
 SQLALCHAMY_DATABASE_URL = 'mysql+mysqlconnector://root:1875mdfm@localhost:3306/fmdeploy_db'
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
