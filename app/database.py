81a2ea68eb545161f1b77d3b3d563f9434c95da8
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2022-02-09 20:41:28 +0000

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-user_name = "user"
+""" user_name = "user"
 password = "2f2kir2y49xy6g"
 host = "db"  
 database_name = "fmdeploy_db"
@@ -18,11 +18,11 @@ engine = create_engine(
     DATABASE,
     encoding="utf-8",
     echo=True
-)
+) """
 
-""" SQLALCHAMY_DATABASE_URL = 'mysql+mysqlconnector://root:1875mdfm@localhost:3306/fmdeploy_db'
+SQLALCHAMY_DATABASE_URL = 'mysql+mysqlconnector://root:1875mdfm@localhost:3306/fmdeploy_db'
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL) #, echo=true """
+engine = create_engine(SQLALCHAMY_DATABASE_URL) #, echo=true
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
